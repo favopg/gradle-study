275605b9784fa0c44f8e2ac06ebff948ea4564cf
favopg <favoriteigo0904@gmail.com>
2020-07-18 10:52:20 +0900

    DBカラム見直しと祝日テーブル作成

--- src/main/resources/db/migration/V1__Create_Appendance.sql
+++ src/main/resources/db/migration/V1__Create_Appendance.sql
@@ -2,22 +2,33 @@
 create table attendance_template(
 	id tinyint primary key not null auto_increment,
 	work_id tinyint not null,
-	start_hour_minute smallint unsigned not null,
-	end_hour_minute smallint unsigned not null,
+	start_hour_minute varchar(4) not null,
+	end_hour_minute varchar(4) not null,
 	rest float not null,
 	over_work float not null,
 	created_at timestamp not null,
 	updated_at timestamp not null
 );
 
--- 勤怠テーブル --
+-- 勤怠テーブル（トランザクション管理）
 create table attendance(
 	id smallint primary key not null auto_increment,
-	year_month_day int not null,
-	start_hour_minute int not null,
-	end_hour_minute int not null,
+	year_and_month varchar(6) not null,
+	day varchar(2) not null,
+	start_hour_minute varchar(4) not null,
+	end_hour_minute varchar(4) not null,
 	rest float not null,
 	over_work float not null,
+	created_at timestamp not null,
+	updated_at timestamp not null
+);
+
+-- 休日テーブル （マスタ管理）
+create table holiday(
+	id smallint primary key not null auto_increment,
+	year_and_month varchar(6) not null,
+	day varchar(2) not null,
+	remark text,
 	created_at timestamp not null,
 	updated_at timestamp not null
 );
